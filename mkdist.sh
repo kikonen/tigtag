7bbb69d686af3ffc124f5adf2fcd805a1657d458
Kari Ikonen <kari@a88-114-59-94.elisa-laajakaista.fi>
2009-04-28 00:42:32 +0300

    BUILD: Initial dist package creation

---
+++ mkdist.sh
@@ -0,0 +1,15 @@
+rm -fr dist
+mkdir dist
+
+jar cvf dist/tick.jar -C ./bin .
+jar uvf dist/tick.jar -C ./resources .
+
+jar cvf dist/kui.jar -C ../kui/bin .
+jar uvf dist/kui.jar -C ../kui/resources .
+
+cp ../kui/lib/*.jar dist
+
+echo "java -cp log4j-1.2.14.jar:TableLayout.jar:kui.jar:tick.jar org.kari.tick.TickMain" > dist/tick.sh
+chmod ugo+x dist/tick.sh
+
+zip -r tick_dist.zip dist/*
