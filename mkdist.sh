d8efcb85cbfc250c8c809ca59bcb35b85c07051f
Kari Ikonen <kari@a88-114-59-94.elisa-laajakaista.fi>
2009-04-30 13:27:54 +0300

    Update dist maker - new libs - initial "M$windows" support

--- mkdist.sh
+++ mkdist.sh
@@ -8,8 +8,11 @@ jar cvf dist/kui.jar -C ../kui/bin .
 jar uvf dist/kui.jar -C ../kui/resources .
 
 cp ../kui/lib/*.jar dist
+cp lib/*.jar dist
 
-echo "java -cp log4j-1.2.14.jar:TableLayout.jar:kui.jar:tick.jar org.kari.tick.TickMain \$@" > dist/tick.sh
+echo "java -cp log4j-1.2.14.jar:TableLayout.jar:jhighlight-1.0.jar:kui.jar:tick.jar org.kari.tick.TickMain \$@" > dist/tick.sh
+echo "start javaw -cp log4j-1.2.14.jar;TableLayout.jar;jhighlight-1.0.jar;kui.jar;tick.jar org.kari.tick.TickMain %1 %2 %3 %4 %5 %6 %7 %8 %9" > dist/tick.bat
+
 chmod ugo+x dist/tick.sh
 
 zip -r tick_dist.zip dist/*
