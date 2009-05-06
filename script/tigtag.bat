141cd093c21310fed02f872799da97b942776606
Kari Ikonen <kari@a88-114-59-94.elisa-laajakaista.fi>
2009-05-06 21:17:28 +0300

    DIST: Added missing trove.jar

--- script/tigtag.bat
+++
@@ -1,3 +0,0 @@
-CLASSPATH=log4j-1.2.14.jar;TableLayout.jar;jsyntaxpane-0.9.5-b27.jar;kui.jar;tigtag.jar
-
-java -cp org.kari.tick.TickMain %1 %2 %3 %4 %5 %6 %7 %8 %9
--- script/tigtag.sh
+++ script/tigtag.sh
@@ -1,4 +1,4 @@
 #
-CLASSPATH=log4j-1.2.14.jar:TableLayout.jar:jsyntaxpane-0.9.5-b27.jar:kui.jar:tigtag.jar
+CLASSPATH=log4j-1.2.14.jar:TableLayout.jar:jsyntaxpane-0.9.5-b27.jar:trove-2.0.4.jar:kui.jar:tigtag.jar
 
 java -cp $CLASSPATH org.kari.tick.TickMain $@
