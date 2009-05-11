0041b11e7c5b8b20a8b6926380bcc85711bb63ac
Kari Ikonen <kari@a88-114-59-94.elisa-laajakaista.fi>
2009-05-12 01:06:32 +0300

    APP: V0.33a

--- src/org/kari/tick/gui/TickConstants.java
+++ src/org/kari/tick/gui/TickConstants.java
@@ -10,7 +10,7 @@ import org.apache.log4j.Logger;
 public interface TickConstants {
     String S = "tick.";
     
-    String VERSION = "0.32 Alpha";
+    String VERSION = "0.33 Alpha";
     String APP_NAME = "TigTag";
     
     Logger LOG = Logger.getLogger("tick");
